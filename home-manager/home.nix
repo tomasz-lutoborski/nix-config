f816f284879740b22ccb74bc576c5984e4f1c062
Tomasz Lutoborski <tomasz@lutoborski.net>
2023-08-22 15:52:34 +0200

    initial commit

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -42,7 +42,11 @@
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+  programs.git = { 
+    enable = true;
+    userName = "Tomasz Lutoborski";
+    userEmail = "tomasz@lutoborski.net";
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
