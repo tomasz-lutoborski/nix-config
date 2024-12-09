00f31be47428446b2cd1476e57ce150157f2bea7
Tomasz Lutoborski <tomasz@lutoborski.net>
2024-12-09 10:11:29 +0100

    tweaks

--- home-manager/apps/logseq.nix
+++ home-manager/apps/logseq.nix
@@ -2,39 +2,47 @@
 { pkgs, ... }: let
   pname = "logseq";
   version = "0.10.9";
-
   src = pkgs.fetchurl {
     url = "https://github.com/logseq/logseq/releases/download/0.10.9/Logseq-linux-x64-0.10.9.AppImage";
     hash = "sha256-XROuY2RlKnGvK1VNvzauHuLJiveXVKrIYPppoz8fCmc=";
   };
   appimageContents = pkgs.appimageTools.extract {inherit pname version src;};
 in
-    pkgs.appimageTools.wrapType2 {
-      inherit pname version src;
-      pkgs = pkgs;
-      extraInstallCommands = ''
-        install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
-        substituteInPlace $out/share/applications/${pname}.desktop \
-          --replace 'Exec=AppRun' 'Exec=${pname}'
-        cp -r ${appimageContents}/usr/share/icons $out/share
+  pkgs.appimageTools.wrapType2 {
+    inherit pname version src;
 
-        # unless linked, the binary is placed in $out/bin/cursor-someVersion
-        # ln -s $out/bin/${pname}-${version} $out/bin/${pname}
-      '';
+    extraInstallCommands = ''
+      # Create necessary directories
+      mkdir -p $out/share/applications
+      mkdir -p $out/share/icons/hicolor/512x512/apps
 
-      extraBwrapArgs = [
-        "--bind-try /etc/nixos/ /etc/nixos/"
-      ];
+      # Create desktop file
+      cat > $out/share/applications/logseq.desktop << EOF
+      [Desktop Entry]
+      Name=Logseq
+      Exec=logseq
+      Terminal=false
+      Type=Application
+      Icon=logseq
+      StartupWMClass=Logseq
+      Comment=A privacy-first, open-source platform for knowledge management and collaboration
+      Categories=Office;
+      EOF
 
-      # vscode likes to kill the parent so that the
-      # gui application isn't attached to the terminal session
-      dieWithParent = false;
+      # Copy icon file (we'll use .DirIcon as fallback if the expected path doesn't exist)
+      if [ -f ${appimageContents}/usr/share/icons/hicolor/512x512/apps/logseq.png ]; then
+        install -m 444 ${appimageContents}/usr/share/icons/hicolor/512x512/apps/logseq.png \
+          $out/share/icons/hicolor/512x512/apps/logseq.png
+      elif [ -f ${appimageContents}/.DirIcon ]; then
+        install -m 444 ${appimageContents}/.DirIcon \
+          $out/share/icons/hicolor/512x512/apps/logseq.png
+      fi
+    '';
 
-      extraPkgs = pkgs: with pkgs; [
-        unzip
-        autoPatchelfHook
-        asar
-        # override doesn't preserve splicing https://github.com/NixOS/nixpkgs/issues/132651
-        (buildPackages.wrapGAppsHook.override {inherit (buildPackages) makeWrapper;})
-      ];
-    }
+    extraPkgs = pkgs: with pkgs; [
+      unzip
+      autoPatchelfHook
+      asar
+      (buildPackages.wrapGAppsHook.override {inherit (buildPackages) makeWrapper;})
+    ];
+  }
